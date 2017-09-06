f0a60f0d56b6e446278203f517016ffd527a93d9
w3b5urf3r <marioslice@gmail.com>
2017-09-06 11:44:34 +1000

    clearance of the handler in the splash (#7)
    * clearance of the handler in the splash
    
    * splash screen refactor
    
    * splash screen fix finish

--- app/src/main/kotlin/com/soho/sohoapp/SplashActivity.kt
+++ app/src/main/kotlin/com/soho/sohoapp/SplashActivity.kt
@@ -2,35 +2,28 @@ package com.soho.sohoapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.view.Window
 import android.view.WindowManager
+import com.soho.sohoapp.feature.home.HomeActivity
 import com.soho.sohoapp.feature.landing.LandingActivity
 import com.soho.sohoapp.helper.SharedPrefsHelper
-import com.soho.sohoapp.feature.home.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
 
-    internal var SPLASH_TIME_OUT: Int = 2000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
-        if (SharedPrefsHelper.getInstance().authToken == "") {
 
-            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
-            setContentView(R.layout.activity_splash)
-            Handler().postDelayed({
-                startActivity(
-                        Intent(this, LandingActivity::class.java))
-            },
-                    SPLASH_TIME_OUT.toLong())
-        } else
-            Handler().postDelayed({
-                startActivity(
-                        Intent(this, HomeActivity::class.java))
-            },
-                    SPLASH_TIME_OUT.toLong())
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        if (SharedPrefsHelper.getInstance().authToken == "") {
+            startActivity(Intent(this, LandingActivity::class.java))
+        } else {
+            startActivity(Intent(this, HomeActivity::class.java))
+        }
+        finish()
     }
+
 }
